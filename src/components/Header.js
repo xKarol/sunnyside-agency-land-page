5e5f2f48e57c749e9750c59b0a10533511ba2f11
xKarol <karolpiskorz8@gmail.com>
2021-11-02 15:42:38 +0100

    ADD: tekst w header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,23 @@ const Header = () => {
     <>
       <div className="header">
         <img className="header__image" src={HeaderImage} alt="" />
+        <div className="header__heading">
+          <div className="details">
+            <h1>We are creatives</h1>
+            <svg width="36" height="114" xmlns="http://www.w3.org/2000/svg">
+              <g
+                stroke="#FFF"
+                strokeWidth="6"
+                fill="none"
+                fillRule="evenodd"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M18 3v100M3 95.484l15 15 15-15" />
+              </g>
+            </svg>
+          </div>
+        </div>
         <div className="header__nav-bar">
           <div className="header__nav">
             <Logo />
