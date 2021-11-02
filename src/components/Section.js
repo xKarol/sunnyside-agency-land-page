47a4abc75bc2748b1c95cb4c984f14243cd25950
xKarol <karolpiskorz8@gmail.com>
2021-11-02 14:28:23 +0100

    ADD: section + opinie

---
+++ src/components/Section.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Section = ({ title, text, src, button, bottom }) => {
+  return (
+    <>
+      <section className="section">
+        {src && <img src={src} alt="" />}
+        {title && text && (
+          <div
+            className={
+              bottom === true ? "section__details bottom" : "section__details"
+            }
+          >
+            {title && <h1 className="section__title">{title}</h1>}
+            {text && <p className="section__text">{text}</p>}
+            {button && (
+              <button
+                className={
+                  button ? `section__button ${button}` : "section__button"
+                }
+              >
+                Learn more
+              </button>
+            )}
+          </div>
+        )}
+      </section>
+    </>
+  );
+};
+
+export default Section;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,77 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import Header from "./components/Header.js";
+import Section from "./components/Section.js";
+import Opinion from "./components/Opinion.js";
+
+import SectionImg1 from "./assets/desktop/image-transform.jpg";
+import SectionImg2 from "./assets/desktop/image-stand-out.jpg";
+import SectionImg3 from "./assets/desktop/image-graphic-design.jpg";
+import SectionImg4 from "./assets/desktop/image-photography.jpg";
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
+    <section>
+      <Section
+        title="Transform your brand"
+        text="We are a full-service creative agency specializing in helping brands grow
+      fast. Engage your clients through compelling visuals that do most of the
+      marketing for you."
+        button="yellow"
+      />
+      <Section src={SectionImg1} />
+      <Section src={SectionImg2} />
+      <Section
+        title="Stand out to the right audience"
+        text="Using a collaborative formula of designers, researchers,
+      photographers, videographers, and copywriters, we’ll build and extend your
+      brand in digital places."
+        button="red"
+      />
+      <Section
+        title="Graphic design"
+        text="Great design makes you memorable. We deliver artwork
+      that underscores your brand message and captures potential clients’
+      attention."
+        src={SectionImg3}
+        bottom={true}
+      />
+      <Section
+        title="Photography"
+        text="Increase your credibility by getting the most
+      stunning, high-quality photos that improve your business image."
+        src={SectionImg4}
+        bottom={true}
+      />
+    </section>
+    <div className="testimonials">
+      <h1>Client Testimonials</h1>
+      <div className="testimonials__section">
+        <Opinion
+          avatar={SectionImg4}
+          name="Emily R."
+          text="We put our trust in Sunnyside and they delivered, making sure our
+            needs were met and deadlines were always hit."
+          job="Marketing Director"
+        />
+        <Opinion
+          avatar={SectionImg4}
+          name="Thomas S."
+          text="Sunnyside’s enthusiasm coupled with their keen interest in our brand’s
+      success made it a satisfying and enjoyable experience."
+          job="Chief Operating Officer"
+        />
+        <Opinion
+          avatar={SectionImg4}
+          name="Jennie F."
+          text="Incredible end result! Our sales increased over 400% when we worked with
+      Sunnyside. Highly recommended!"
+          job="Business Owner"
+        />
+      </div>
+      About Services Projects Contact
+    </div>
   </React.StrictMode>,
   document.getElementById("root")
 );
